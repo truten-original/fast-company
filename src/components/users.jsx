c21e7c2aeb8ee720af8aec75867b1c266a25ef8b
Roman Trutnev <trutenoriginal@gmail.com>
2022-07-05 13:15:15 +0300

    speaker version

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,83 +1,78 @@
-import { useState } from "react"
-import API from "../api"
+import React, { useState } from "react"
+import api from "../api"
+
 const Users = () => {
-  const [users, setUsers] = useState(API.users.fetchAll())
-  const renderList = (users) => {
-    return users.map((user) => {
-      return (
-        <tr key={user["_id"]}>
-          <td>{user.name}</td>
-          <td>
-            {user.qualities.map((quality) => {
-              return (
-                <span
-                  key={quality["_id"]}
-                  className={`badge  bg-${quality.color}`}
-                  style={{ marginLeft: "4px" }}
-                >
-                  {quality.name}
-                </span>
-              )
-            })}
-          </td>
-          <td>{user.profession.name}</td>
-          <td>{user.completedMeetings}</td>
-          <td>{user.rate}/5</td>
-          <td>
-            <button
-              className="bg-danger btn text-white"
-              data-user={user["_id"]}
-              onClick={(event) => {
-                const dataId = event.target.getAttribute("data-user")
-                handleDelete(dataId)
-              }}
-            >
-              delete
-            </button>
-          </td>
-        </tr>
-      )
-    })
-  }
+  const [users, setUsers] = useState(api.users.fetchAll())
+
   const handleDelete = (userId) => {
-    setUsers([...users].filter((user) => user["_id"] !== userId))
+    setUsers(users.filter((user) => user._id !== userId))
   }
+
   const renderPhrase = (number) => {
-    let phrase
-    if (number > 4 || number === 1) {
-      phrase = `${number} человек тусанет с тобой сегодня`
-    } else if (number > 1 && number <= 4) {
-      phrase = `${number} человека тусанут с тобой сегодня`
-    }
-    return phrase
+    const lastOne = Number(number.toString().slice(-1))
+    if (number > 4 && number < 15) return "человек тусанет"
+    if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут"
+    if (lastOne === 1) return "человек тусанет"
+    return "человек тусанет"
   }
-  if (users.length)
-    return (
-      <>
-        <h1
-          className="bg-primary text-white p-2"
-          style={{ width: "fit-content" }}
+
+  return (
+    <>
+      <h2>
+        <span
+          className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
         >
-          {renderPhrase(users.length)}
-        </h1>
+          {users.length > 0
+            ? `${
+                users.length + " " + renderPhrase(users.length)
+              } с тобой сегодня`
+            : "Никто с тобой не тусанет"}
+        </span>
+      </h2>
+
+      {users.length > 0 && (
         <table className="table">
           <thead>
             <tr>
-              <th>Имя</th>
-              <th>Качества</th>
-              <th>Профессия</th>
-              <th>Встретился, раз</th>
-              <th>Оценка</th>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
             </tr>
           </thead>
-          <tbody>{renderList(users)}</tbody>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((item) => (
+                    <span
+                      className={"badge m-1 bg-" + item.color}
+                      key={item._id}
+                    >
+                      {item.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate} /5</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-danger"
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      </>
-    )
-  return (
-    <h1 className="bg-danger text-white p-2" style={{ width: "fit-content" }}>
-      Никто с тобой не тусанет
-    </h1>
+      )}
+    </>
   )
 }
 
